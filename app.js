4e7944eaadc00cd3cf6c45b7681070cede0b42dd
vijaymishra13 <vijay.mishra13@gmail.com>
2016-12-22 17:14:45 -0500

    Changes for the first push
    First push to GIT with all the changes

---
+++ app.js
@@ -0,0 +1,139 @@
+var express = require('express')
+  , http = process.env.HTTPS == 'on' ? require('https') : require('http')
+  , builder = require('botbuilder');
+
+var app = express()
+  , server = http.createServer(app)
+  , port = process.env.port || 3000
+  , config = { appId: '1dbd54e3-dec9-4ae2-b56e-bb5b8b56502d', appPassword: 'ZA2UsjDhZUt4saUWCv3RmPO' }
+  , connector = new builder.ChatConnector(config)
+  , bot = new builder.UniversalBot(connector);
+
+
+// define app HTTP routes
+app
+
+// respond to basic HTTP GET
+  .get('/', function(req, res) {
+    res.send('hello world');
+  })
+
+  // respond to bot messages
+  .post('/api/messages', connector.listen());
+
+// start the server
+server.listen(port, function() {
+  console.log('Listening on %s', port);
+});
+
+// Create LUIS recognizer that points at our model and add it as the root '/' dialog for our Cortana Bot.
+var model = 'https://api.projectoxford.ai/luis/v2.0/apps/c413b2ef-382c-45bd-8ff0-f76d60e2a821?subscription-key=e86a03e6ed394f1098da68f85f380f8e';
+var recognizer = new builder.LuisRecognizer(model);
+var dialog = new builder.IntentDialog({ recognizers: [recognizer] });
+
+bot.dialog('/', dialog);
+
+// Add intent handlers
+// Add intent handlers
+dialog.matches('builtin.intent.alarm.set_alarm', [
+  function (session, args, next) {
+    // Resolve and store any entities passed from LUIS.
+    var title = builder.EntityRecognizer.findEntity(args.entities, 'builtin.alarm.title');
+    var time = builder.EntityRecognizer.resolveTime(args.entities);
+    var alarm = session.dialogData.alarm = {
+      title: title ? title.entity : null,
+      timestamp: time ? time.getTime() : null
+    };
+
+    // Prompt for title
+    if (!alarm.title) {
+      builder.Prompts.text(session, 'What would you like to call your alarm?');
+    } else {
+      next();
+    }
+  },
+  function (session, results, next) {
+    var alarm = session.dialogData.alarm;
+    if (results.response) {
+      alarm.title = results.response;
+    }
+
+    // Prompt for time (title will be blank if the user said cancel)
+    if (alarm.title && !alarm.timestamp) {
+      builder.Prompts.time(session, 'What time would you like to set the alarm for?');
+    } else {
+      next();
+    }
+  },
+  function (session, results) {
+    var alarm = session.dialogData.alarm;
+    if (results.response) {
+      var time = builder.EntityRecognizer.resolveTime([results.response]);
+      alarm.timestamp = time ? time.getTime() : null;
+    }
+
+    // Set the alarm (if title or timestamp is blank the user said cancel)
+    if (alarm.title && alarm.timestamp) {
+      // Save address of who to notify and write to scheduler.
+      alarm.address = session.message.address;
+      alarms[alarm.title] = alarm;
+
+      // Send confirmation to user
+      var date = new Date(alarm.timestamp);
+      var isAM = date.getHours() < 12;
+      session.send('Creating alarm named "%s" for %d/%d/%d %d:%02d%s',
+        alarm.title,
+        date.getMonth() + 1, date.getDate(), date.getFullYear(),
+        isAM ? date.getHours() : date.getHours() - 12, date.getMinutes(), isAM ? 'am' : 'pm');
+    } else {
+      session.send('Ok... no problem.');
+    }
+  }
+]);
+
+dialog.matches('builtin.intent.alarm.delete_alarm', [
+  function (session, args, next) {
+    // Resolve entities passed from LUIS.
+    var title;
+    var entity = builder.EntityRecognizer.findEntity(args.entities, 'builtin.alarm.title');
+    if (entity) {
+      // Verify its in our set of alarms.
+      title = builder.EntityRecognizer.findBestMatch(alarms, entity.entity);
+    }
+
+    // Prompt for alarm name
+    if (!title) {
+      builder.Prompts.choice(session, 'Which alarm would you like to delete?', alarms);
+    } else {
+      next({ response: title });
+    }
+  },
+  function (session, results) {
+    // If response is null the user canceled the task
+    if (results.response) {
+      delete alarms[results.response.entity];
+      session.send("Deleted the '%s' alarm.", results.response.entity);
+    } else {
+      session.send('Ok... no problem.');
+    }
+  }
+]);
+
+dialog.onDefault(builder.DialogAction.send("I'm sorry I didn't understand. I can only create & delete alarms."));
+
+// Very simple alarm scheduler
+var alarms = {};
+setInterval(function () {
+  var now = new Date().getTime();
+  for (var key in alarms) {
+    var alarm = alarms[key];
+    if (now >= alarm.timestamp) {
+      var msg = new builder.Message()
+        .address(alarm.address)
+        .text("Here's your '%s' alarm.", alarm.title);
+      bot.send(msg);
+      delete alarms[key];
+    }
+  }
+}, 15000);
+
---
+++ nodeStart.bat
@@ -0,0 +1,5 @@
+
+set http_proxy=http://genproxy:8080
+set no_proxy=localhost,127.0.0.0/8,10.0.0.0/8,172.16.0.0/12,192.168.0.0/16
+
+node app.js
